8c8871457b1a5f349cc4415e1acb81a6b378c731
AdeotiDev Al-Faoz <adeotitempo@gmail.com>
2026-03-17 07:56:40 +0100

    Completed the bulk-print b...

---
+++ database/migrations/2026_03_17_065304_add_result_root_to_attendances.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\ResultRoot;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('attendances', function (Blueprint $table) {
+            //
+            $table->foreignIdFor(ResultRoot::class)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('attendances', function (Blueprint $table) {
+            //
+            $table->dropForeign(['result_root_id']);
+            $table->dropColumn('result_root_id');
+        });
+    }
+};
